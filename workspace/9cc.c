8642cf960ee2be3a6fdb0708604afa88de41b77c
k.okabe <kyota@keokabenoMacBook-Pro.local>
2022-01-29 19:41:03 +0900

    unary plus/minus

--- workspace/9cc.c
+++ workspace/9cc.c
@@ -39,6 +39,7 @@ struct Node {
 
 Node *primary();
 Node *expr();
+Node *unary();
 Node *mul();
 
 // 入力プログラム
@@ -167,17 +168,27 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*')) {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         } else if (consume('/')) {
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         } else {
             return node;
         }
     }
+}
+
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    } else if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+
+    return primary();
 }
 
 Node *primary() {
--- workspace/test.sh
+++ workspace/test.sh
@@ -26,4 +26,7 @@ assert 47 "5+6*7"
 assert 15 "5*(9-6)"
 assert 4 "(3+5)/2"
 
+assert 10 "-10+20"
+assert 5 "+20-15"
+
 echo OK
